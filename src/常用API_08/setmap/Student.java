3d26c60afdecc983b18d1c8c3e902188163500ca
zhaobao1830 <x5219438>
2019-04-17 17:14:59 +0800

    Student类

---
+++ src/常用API_08/setmap/Student.java
@@ -0,0 +1,59 @@
+package 常用API_08.setmap;
+
+import java.util.Objects;
+
+/**
+ * Student类
+ */
+public class Student {
+    private int age;
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        System.out.println("我是equals方法");
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println("我是hashCode");
+        return Objects.hash(age, name);
+    }
+}
